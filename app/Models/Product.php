5e258c12f03b3e86f1a529da1298298612cd50cc
unknown <zain.suleman1@icloud.com>
2026-02-06 00:19:48 +0500

    added tags relationship

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,4 +14,8 @@ class Product extends Model
     public function category(){
         return $this->belongsTo(Category::class);
     }
+
+    public function tags(){
+        return $this->belongsToMany(Tag::class);
+    }
 }
